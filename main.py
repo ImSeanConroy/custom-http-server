486e4168944d33d3c489ffe39004a8e62d958589
Sean Conroy <seanconroy2001@gmail.com>
2026-01-04 20:44:39 +0000

    Implement 200 status response

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen(5)
 
-print(f"Listening on {HOST}:{PORT}")
-
 while True:
     client_socket, addr = server_socket.accept()
     print("Connection from", addr)
+
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    client_socket.sendall(response.encode())
+
     client_socket.close()
